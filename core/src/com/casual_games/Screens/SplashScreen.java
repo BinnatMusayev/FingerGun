2d3458941595b7f8dacdfc37e17472bd710adc47
Binnat Musayev <binnat.musayev@sap.com>
2018-11-21 09:12:00 +0100

    removing unnecessary code

--- core/src/com/casual_games/Screens/SplashScreen.java
+++ core/src/com/casual_games/Screens/SplashScreen.java
@@ -39,7 +39,6 @@ public class SplashScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
 
         if (TimeUtils.millis() - splashScreenTimeout > 2500){
-//            game.setScreen(new MainMenu(game));
             game.setScreen(game.mainMenu);
         }else{
 
